44ccfb147fd4de6fa1da84cdfe2b05c00f8fb2e4
kafkiansky <vadimzanfir@gmail.com>
2026-01-23 19:02:32 +0300

    chore: StreamCodec both for client and server streams

---
+++ src/Internal/Http2/StreamCodec.php
@@ -0,0 +1,106 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Thesis\Grpc\Internal\Http2;
+
+use Amp\Pipeline;
+use Revolt\EventLoop;
+use Thesis\Grpc\Compression\Compressor;
+use Thesis\Grpc\Encoding\Encoder;
+use Thesis\Grpc\Internal\Protocol;
+
+/**
+ * @internal
+ */
+final readonly class StreamCodec
+{
+    public function __construct(
+        private Encoder $encoder,
+        private Compressor $compressor,
+    ) {}
+
+    /**
+     * @template T of object
+     * @param Pipeline\ConcurrentIterator<T> $in
+     * @return Pipeline\ConcurrentIterator<non-empty-string>
+     */
+    public function encode(Pipeline\ConcurrentIterator $in): Pipeline\ConcurrentIterator
+    {
+        /** @var Pipeline\Queue<non-empty-string> $out */
+        $out = new Pipeline\Queue();
+
+        $encoder = $this->encoder;
+        $compressor = $this->compressor;
+
+        EventLoop::queue(static function () use (
+            $encoder,
+            $compressor,
+            $in,
+            $out,
+        ): void {
+            foreach ($in as $message) {
+                $buffer = $compressed = $encoder->encode($message);
+
+                if ($buffer !== '') {
+                    $compressed = $compressor->compress($buffer);
+                }
+
+                $frame = Protocol\encodeFrame(new Protocol\Frame(
+                    $compressed !== $buffer,
+                    $compressed,
+                ));
+
+                $out->push($frame);
+            }
+
+            $out->complete();
+        });
+
+        return $out->iterate();
+    }
+
+    /**
+     * @template T of object
+     * @param \Traversable<string> $in
+     * @param class-string<T> $type
+     * @return Pipeline\ConcurrentIterator<T>
+     */
+    public function decode(\Traversable $in, string $type): Pipeline\ConcurrentIterator
+    {
+        /** @var Pipeline\Queue<T> $out */
+        $out = new Pipeline\Queue();
+
+        $encoder = $this->encoder;
+        $compressor = $this->compressor;
+
+        EventLoop::queue(static function () use (
+            $encoder,
+            $compressor,
+            $in,
+            $out,
+            $type,
+        ): void {
+            /** @var string $message */
+            foreach ($in as $message) {
+                if ($message === '') {
+                    continue;
+                }
+
+                $frame = Protocol\decodeFrame($message);
+
+                $buffer = $frame->buffer;
+
+                if ($frame->compressed && $buffer !== '') {
+                    $buffer = $compressor->decompress($buffer);
+                }
+
+                $out->push($encoder->decode($buffer, $type));
+            }
+
+            $out->complete();
+        });
+
+        return $out->iterate();
+    }
+}
