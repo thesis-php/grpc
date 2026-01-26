b1eb04ccc15c6eca992fc11e240aa8fa7afe832a
kafkiansky <vadimzanfir@gmail.com>
2026-01-26 10:08:03 +0300

    chore: BidirectionalStreamHandler and Channel implemented

---
+++ src/Server/BidirectionalStreamChannel.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Thesis\Grpc\Server;
+
+use Thesis\Grpc\ServerStream;
+
+/**
+ * @api
+ * @template TRequest of object
+ * @template TResponse of object
+ * @template-implements \IteratorAggregate<array-key, TRequest>
+ */
+final readonly class BidirectionalStreamChannel implements \IteratorAggregate
+{
+    /**
+     * @param ServerStream<TRequest, TResponse> $stream
+     */
+    public function __construct(
+        private ServerStream $stream,
+    ) {}
+
+    /**
+     * @param TResponse $request
+     */
+    public function send(object $request): void
+    {
+        $this->stream->send($request);
+    }
+
+    /**
+     * @return TRequest
+     */
+    public function receive(): object
+    {
+        return $this->stream->receive();
+    }
+
+    public function close(): void
+    {
+        $this->stream->close();
+    }
+
+    #[\Override]
+    public function getIterator(): \Traversable
+    {
+        return $this->stream->getIterator();
+    }
+}
---
+++ src/Server/BidirectionalStreamHandler.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Thesis\Grpc\Server;
+
+use Amp\Cancellation;
+use Thesis\Grpc\ServerStream;
+
+/**
+ * @api
+ * @template TRequest of object
+ * @template TResponse of object
+ * @template-implements Handler<TRequest, TResponse>
+ */
+final readonly class BidirectionalStreamHandler implements Handler
+{
+    /**
+     * @param \Closure(BidirectionalStreamChannel<TRequest, TResponse>, Cancellation): void $handler
+     */
+    public function __construct(
+        private \Closure $handler,
+    ) {}
+
+    #[\Override]
+    public function handle(ServerStream $stream, Cancellation $cancellation): void
+    {
+        ($this->handler)(new BidirectionalStreamChannel($stream), $cancellation);
+    }
+}
